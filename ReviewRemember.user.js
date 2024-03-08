d8881d3349d1485f5f4e115dc50901a7e13bc121
TeiTong <159563030+TeiTong@users.noreply.github.com>
2024-03-08 17:46:16 +0100

    Update ReviewRemember.user.js

--- ReviewRemember.user.js
+++ ReviewRemember.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         ReviewRemember
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Sauvegarde des avis Amazon
-// @author       MegaMan
+// @version      1.0
+// @description  Outils pour les avis Amazon
+// @author       Ashemka et MegaMan
 // @match        https://www.amazon.fr/review/create-review*
+// @match        https://www.amazon.fr/vine/vine-reviews*
+// @match        https://www.amazon.fr/vine/account
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.fr
 // @updateURL    https://raw.githubusercontent.com/teitong/reviewremember/main/ReviewRemember.user.js
 // @downloadURL  https://raw.githubusercontent.com/teitong/reviewremember/main/ReviewRemember.user.js
@@ -14,21 +16,17 @@
 window.onload = function() {
     'use strict';
 
-    //Ajout du menu
-    GM_registerMenuCommand("Supprimer le modèle d'avis", function() {
-        deleteTemplate();
-        reloadButtons();
-        alert("Modèle d'avis supprimé.");
-    }, "m");
-
-     GM_registerMenuCommand("Supprimer tous les avis", function() {
-        deleteAllReviews();
-         reloadButtons();
-        alert("Tous les avis ont été supprimés.");
-    }, "a");
-
     const asin = new URLSearchParams(window.location.search).get('asin');
 
+    //Suppression footer
+    var styleFooter = document.createElement('style');
+
+    styleFooter.textContent = `
+#rhf, #rhf-shoveler, .rhf-frame, #navFooter {
+  display: none !important;
+}
+`
+    document.head.appendChild(styleFooter);
     // Définition des styles pour les boutons
     const styles = `
         .custom-button {
@@ -190,16 +188,249 @@ window.onload = function() {
             }
         });
     }
-
     addButtons();
+    //Fonctions pour les couleurs des avis
+    // Fonction pour changer la couleur de la barre en fonction du pourcentage
+    function changeColor() {
+        var progressBar = document.querySelector('#vvp-perc-reviewed-metric-display .animated-progress span');
+        var progressValue = parseFloat(progressBar.getAttribute('data-progress'));
 
-    //Suppression footer
-    var styleFooter = document.createElement('style');
+        var color = '';
+        var width = progressBar.style.width;
+        if (progressValue < 60) {
+            color = 'red';
+        } else if (progressValue >= 60 && progressValue < 90) {
+            color = 'orange';
+        } else {
+            color = '#32cd32';
+        }
 
-    styleFooter.textContent = `
-#rhf, #rhf-shoveler, .rhf-frame, #navFooter {
-  display: none !important;
+        progressBar.style.backgroundColor = color;
+        progressBar.style.width = width;
+    }
+
+    // Fonction pour formater une date en format 'DD/MM/YYYY'
+    function formatDate(date) {
+        var day = date.getDate().toString().padStart(2, '0');
+        var month = (1 + date.getMonth()).toString().padStart(2, '0');
+        var year = date.getFullYear();
+
+        return day + '/' + month + '/' + year;
+    }
+
+    // Fonction pour calculer la différence en jours entre deux dates
+    function dateDiffInDays(date1, date2) {
+        const diffInTime = date2.getTime() - date1.getTime();
+        return Math.floor(diffInTime / (1000 * 3600 * 24));
+    }
+
+    //Style pour "Pas encore examiné"
+    var styleReview = document.createElement('style');
+    styleReview.textContent = `
+        .pending-review-blue {
+    font-weight: bold;
+    color: #0000FF !important;
 }
-`
-    document.head.appendChild(styleFooter);
+        .pending-review-green {
+    font-weight: bold;
+    color: #008000 !important;
+}
+        .pending-review-orange {
+    font-weight: bold;
+    color: #FFA500 !important;
+}
+        .pending-review-red {
+    font-weight: bold;
+    color: #FF0000 !important;
+}
+    `;
+    document.head.appendChild(styleReview);
+    // Fonction pour mettre en surbrillance les dates en fonction de leur âge
+    function highlightDates() {
+        if (window.location.href.includes('review-type=completed') || window.location.href.includes('orders')) {
+            return; // Ne rien faire si l'URL contient "review-type=completed" ou "orders"
+        }
+
+        var tdElements = document.querySelectorAll('.vvp-reviews-table--text-col');
+        var currentDate = new Date();
+
+        tdElements.forEach(function(td, index, array) {
+            var timestamp = parseInt(td.getAttribute('data-order-timestamp'));
+            if (td.hasAttribute('data-order-timestamp')) {
+                var nextTd = array[index + 1];
+                // Vérifier si le timestamp est en millisecondes et le convertir en secondes si nécessaire
+                if (timestamp > 1000000000000) {
+                    timestamp /= 1000; // Conversion en secondes
+                }
+
+                var date = new Date(timestamp * 1000); // Convertir le timestamp en millisecondes avant de créer l'objet Date
+
+                var daysDifference = dateDiffInDays(date, currentDate);
+
+                var formattedDate = formatDate(date);
+
+                //var style = '';
+                //var color = '';
+                if (daysDifference < 7) {
+                    //color = '#0000FF'; // bleu
+                    nextTd.classList.add('pending-review-blue');
+                } else if (daysDifference >= 7 && daysDifference < 14) {
+                    //color = '#008000'; // vert
+                    nextTd.classList.add('pending-review-green');
+                } else if (daysDifference >= 14 && daysDifference < 30) {
+                   //color = '#FFA500'; // orange
+                    nextTd.classList.add('pending-review-orange');
+                } else {
+                    //color = '#FF0000'; // rouge
+                    nextTd.classList.add('pending-review-red');
+                }
+
+                // Ajouter la couleur et le style gras au texte de la date
+                //style = 'font-weight: bold; color: ' + color + ';';
+                //td.innerHTML = '<font style="' + style + '">' + formattedDate + '</font>';
+            }
+        });
+    }
+
+    // Fonction pour mettre en surbrillance le statut de la revue
+    function highlightReviewStatus() {
+        var enableReviewStatusFunction = localStorage.getItem('enableReviewStatusFunction');
+
+        if (enableReviewStatusFunction === 'true') {
+            var tdElements = document.querySelectorAll('td.vvp-reviews-table--text-col');
+
+            tdElements.forEach(function(td) {
+                var reviewStatus = td.innerText.trim();
+                var style = '';
+
+                switch (reviewStatus) {
+                    case 'En attente d\'approbation':
+                        style += 'font-weight: bold; color: #FFA500;'; // orange
+                        break;
+                    case 'Approuvé':
+                        style += 'font-weight: bold; color: #008000;'; // vert
+                        break;
+                    case 'Non approuvé':
+                        style += 'font-weight: bold; color: #FF0000;'; // rouge
+                        break;
+                    case 'Vous avez commenté cet article':
+                        style += 'font-weight: bold; color: #0000FF;'; // bleu
+                        break;
+                    default:
+                        style += 'color: inherit;'; // utiliser la couleur par défaut
+                }
+
+                // Appliquer le style au texte de la revue
+                td.style = style;
+            });
+        }
+    }
+
+    // Fonction pour mettre en surbrillance le statut "Cet article n'est plus disponible"
+    function highlightUnavailableStatus() {
+        var divElements = document.querySelectorAll('div.vvp-subtitle-color');
+
+        divElements.forEach(function(div) {
+            var subtitle = div.innerText.trim();
+
+            if (subtitle === "Cet article n'est plus disponible") {
+                div.style.fontWeight = 'bold';
+                div.style.color = '#FF0000'; // rouge
+            }
+        });
+    }
+
+    // Fonction pour activer ou désactiver la fonction de date
+    function toggleDateFunction() {
+        var enableDateFunction = localStorage.getItem('enableDateFunction');
+
+        if (enableDateFunction === 'true') {
+            localStorage.setItem('enableDateFunction', 'false');
+        } else {
+            localStorage.setItem('enableDateFunction', 'true');
+        }
+        location.reload();
+    }
+
+    // Fonction pour activer ou désactiver la fonction de statuts des commentaires
+    function toggleReviewStatusFunction() {
+        var enableReviewStatusFunction = localStorage.getItem('enableReviewStatusFunction');
+
+        if (enableReviewStatusFunction === 'true') {
+            localStorage.setItem('enableReviewStatusFunction', 'false');
+        } else {
+            localStorage.setItem('enableReviewStatusFunction', 'true');
+        }
+        location.reload();
+    }
+
+    // Fonction pour activer ou désactiver la coloration de la barre de progression
+    function toggleColorFunction() {
+        var enableColorFunction = localStorage.getItem('enableColorFunction');
+
+        if (enableColorFunction === 'true') {
+            localStorage.setItem('enableColorFunction', 'false');
+        } else {
+            localStorage.setItem('enableColorFunction', 'true');
+        }
+        location.reload();
+    }
+
+    //localStorage.removeItem('enableDateFunction');
+    //localStorage.removeItem('enableReviewStatusFunction');
+    //localStorage.removeItem('enableColorFunction');
+    var enableDateFunction = localStorage.getItem('enableDateFunction');
+    var enableReviewStatusFunction = localStorage.getItem('enableReviewStatusFunction');
+    var enableColorFunction = localStorage.getItem('enableColorFunction');
+
+    // Initialiser à true si la clé n'existe pas dans le stockage local
+    if (enableDateFunction === null) {
+        enableDateFunction = 'true';
+        localStorage.setItem('enableDateFunction', enableDateFunction);
+    }
+
+    if (enableReviewStatusFunction === null) {
+        enableReviewStatusFunction = 'true';
+        localStorage.setItem('enableReviewStatusFunction', enableReviewStatusFunction);
+    }
+
+    if (enableColorFunction === null) {
+        enableColorFunction = 'true';
+        localStorage.setItem('enableColorFunction', enableColorFunction);
+    }
+
+    if (enableDateFunction === 'true') {
+        highlightDates();
+        GM_registerMenuCommand("Désactiver le surlignage du statut des avis", toggleDateFunction);
+    } else {
+        GM_registerMenuCommand("Activer le surlignage du statut des avis", toggleDateFunction);
+    }
+
+    if (enableReviewStatusFunction === 'true') {
+        highlightReviewStatus();
+        GM_registerMenuCommand("Désactiver le surlignage des avis vérifiés", toggleReviewStatusFunction);
+    } else {
+        GM_registerMenuCommand("Activer le surlignage des avis vérifiés", toggleReviewStatusFunction);
+    }
+
+    if (enableColorFunction === 'true') {
+        changeColor();
+        GM_registerMenuCommand("Désactiver le changement de couleur de la barre de progression des avis", toggleColorFunction);
+    } else {
+        GM_registerMenuCommand("Activer le changement de couleur de la barre de progression des aviss", toggleColorFunction);
+    }
+    //End
+    //Ajout du menu
+    GM_registerMenuCommand("Supprimer le modèle d'avis", function() {
+        deleteTemplate();
+        reloadButtons();
+        alert("Modèle d'avis supprimé.");
+    }, "m");
+
+    GM_registerMenuCommand("Supprimer tous les avis", function() {
+        deleteAllReviews();
+        reloadButtons();
+        alert("Tous les avis ont été supprimés.");
+    }, "a");
+
 };
